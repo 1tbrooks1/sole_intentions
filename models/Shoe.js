a012abb376987a6ae584f8e8a63549892b99af70
tbrooks <tbrooks321@yahoo.com>
2022-03-02 20:48:28 -0500

    created shoe model

--- models/Shoe.js
+++ models/Shoe.js
@@ -3,19 +3,32 @@ const sequelize = require("../config/connection");
 
 class Shoe extends Model {}
 
-Shoe.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
+Shoe.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     price: {
-    type: DataTypes.DECIMAL,
-    allowNull: false,
-}
-});
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "shoe",
+  }
+);
+
+module.exports = Shoe;
