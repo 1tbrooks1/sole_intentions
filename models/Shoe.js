51b0ff78dd567a1c3cf245d9063e7bd8763c84ae
tbrooks <tbrooks321@yahoo.com>
2022-03-02 20:40:15 -0500

    created user model

---
+++ models/Shoe.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Shoe extends Model {}
+
+Shoe.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+    price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+}
+});
