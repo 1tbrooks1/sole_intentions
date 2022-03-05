22375b5f70d2a74f4c350944448e9f1f47a76123
tbrooks <tbrooks321@yahoo.com>
2022-03-04 20:41:53 -0500

    added images to public folder and worked on shoe seeding

--- seeds/shoes.js
+++ seeds/shoes.js
@@ -2,6 +2,13 @@ const { Shoe } = require('../models');
 
 const shoeData = [
     {
+        name: 'Nike Air Max 1 Atmos Safari',
+        price: '610',
+        description: `The Atmos x Air Max 1 'Safari' is a 2016 re-release of a classic 2002 collaboration 
+        with Japanese brand Atmos. The reissue features the same color-blocking as the original but swaps 
+        out the 2002s canvas toe box and gum sole for a hairy suede toe box and ice-blue translucent sole.`
+    },
+    {
         name: 'Adidas Yeezy Boost 350 V2 Beluga',
         price: '575',
         description: `The adidas Yeezy 350 Boost V2 takes the silhouette that 
@@ -37,12 +44,28 @@ const shoeData = [
         and pink, this pair is sure to turn heads with its unique and vibrant style.`
     },
     {
+        name: 'Air Jordan 1 Rookie of the Year',
+        price: '665',
+        description: `What better way to celebrate MJs rookie of the year award than clinching 
+        the Jordan 1 Retro High Rookie of the Year. This unique shoe comes with a white and 
+        harvest leather upper, black Nike “Swoosh”, white midsole, and a black sole.`
+    },
+    {
         name: 'Air Jordan 1 Shattered Backboard',
         price: '1600',
         description: `The name “Shattered Backboard” comes from a moment after Michael Jordans 
         rookie season, in the summer of 1985, when he played in an exhibition game in Italy. 
         Jordan, wearing the orange, black, and white of Stefanel Trieste, hit a dunk so hard 
         he shattered the glass backboard.`
+    },
+    {
+        name: 'Nike x Stussy Air Zoom Spiridon Fossil',
+        price: '565',
+        description: `Stussy teams up with Nike once again to deliver the Nike Air Zoom Spiridon 
+        Cage 2 Stussy Fossil. After originally debuting in 2003, this design received a refresh 
+        with the help of lifestyle label Stussy. This Spridion Cage 2 consists of a mesh upper with 
+        woven textile overlays and a black Swoosh. A caged Zoom Air unit, Stussy branding on the heel 
+        and toe, and Fossil sole completes the design.`
     },
     {
         name: 'Adidas Yeezy Boost 350 V2 Static',
@@ -60,6 +83,16 @@ const shoeData = [
         was exclusively sold through Supreme stores and their online shops.`
     },
     {
+        name: 'Adidas Yeezy Boost 700 Wave Runner',
+        price: '400',
+        description: `First releasing in November of 2017, the Yeezy Boost 700s represented what 
+        was a significant shift in Kanyes design aesthetic, moving from the minimalistic silhouettes 
+        of early Yeezy seasons to this chunky runner model. It once again showed how Kanye stays ahead 
+        of the curve, as chunkier sneakers became more en vogue in 2018. The shoe features an upper with 
+        grey and black suede overlays, premium leather with blue mesh underlays, neon green laces, and its 
+        signature chunky midsole with encapsulated Boost technology.`
+    },
+    {
         name: 'Adidas Yeezy Boost 350 V2 Zebra',
         price: '425',
         description: `Adidas is back with their latest Yeezy Boost 350 V2. Fresh off the heels of 
@@ -68,26 +101,6 @@ const shoeData = [
         off a Zebra stripe vibe, “SPLY-350” displayed across the sides in red finished off by a translucent 
         BOOST cushioned sole. To date they are the most limited adidas Yeezy release and have instantly become 
         one of the most popular colorways`
-    },
-    {
-        name: '',
-        price: '',
-        description: ''
-    },
-    {
-        name: '',
-        price: '',
-        description: ''
-    },
-    {
-        name: '',
-        price: '',
-        description: ''
-    },
-    {
-        name: '',
-        price: '',
-        description: ''
     },
 ]
 
