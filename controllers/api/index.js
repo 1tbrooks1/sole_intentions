6fc9c4dc5fbc126c0ad93cdf2c9c5bdc3bf0606f
marissann11 <wilhite.marissa@gmail.com>
2022-03-05 10:33:44 -0500

    seeds and cart item

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const shoeRoutes = require('./shoe-routes');
 const cartRoutes = require('./cart-routes');
-const cartItemRoutes = require('./cartItem-routes');
+// const cartItemRoutes = require('./cartItem-routes');
 
 router.use('/user', userRoutes);
 router.use('/shoe', shoeRoutes);
 router.use('/cart', cartRoutes);
-router.use('/cartitem', cartItemRoutes);
+// router.use('/cartitem', cartItemRoutes);
 
 module.exports = router;
