bb7e5999d44c123c817f53708767116523f9c851
marissann11 <wilhite.marissa@gmail.com>
2022-03-05 10:10:07 -0500

    touch ups and cart item file added

--- models/index.js
+++ models/index.js
@@ -11,15 +11,18 @@ User.hasOne(Cart, {
   foreignKey: 'cart',
 });
 
-Cart.belongsToMany(Shoe, { through: 'CartItem' });
+Cart.belongsToMany(Shoe, {
+  through: 'CartItem',
+});
 
-Shoe.belongsToMany(Cart, { through: 'CartItem' });
+Shoe.belongsToMany(Cart, {
+  through: 'CartItem',
+});
 
 // User has one cart
 // Cart has one user
 // Shoe belongs to cart ?
 // Shoe belongs to user ?
-// Would a shoe have many carts ?
 // do we have to add a cart FK to shoe Model for if a shoe has a cart
 // User model needs FK for Cart
 // Cart model needs FK for User (it's owner)
