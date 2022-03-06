94c71d16e05bc6d53aa88cfaae1567ad37e5704d
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-03-06 01:39:50 -0500

    inital login js logic

---
+++ public/javascript/login.js
@@ -0,0 +1,55 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/user/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const address = document.querySelector('#address-signup').value.trim();
+
+    if (username && email && password && address) {
+        const response = await fetch('/api/user', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password,
+                address
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-button').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-button').addEventListener('submit', signupFormHandler);
