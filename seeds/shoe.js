54079aabf373c69acaa087eeaf278b7d2c34f4a8
tbrooks <tbrooks321@yahoo.com>
2022-03-08 21:42:21 -0500

    updated and created seeds for current and new shoes

--- seeds/shoe.js
+++ seeds/shoe.js
@@ -2,6 +2,19 @@ const { Shoe } = require('../models');
 
 const shoeData = [
   {
+    name: 'Adidas Yeezy Boost 380 Alein',
+    price: 310,
+    description: `Yeezy has introduced a new silhouette to their product line with the adidas Yeezy Boost 
+    380 Alien, now available on StockX. This model was originally known to be the Yeezy Boost 350 V3, 
+    but upon release it was given its own name. This 380 Alien features a never before seen Primeknit 
+    pattern on its upper and lacks the traditional lateral side strip. An upgraded thicker Boost midsole 
+    and engineered outsole grip complete the design.`,
+    year: '2019',
+    filename: 'Alien.jpg',
+    stripe_prod_id: '',
+    stripe_price_id: '',
+  },
+  {
     name: 'Nike Air Max 1 Atmos Safari',
     price: 610,
     description: `The Atmos x Air Max 1 'Safari' is a 2016 re-release of a classic 2002 collaboration 
@@ -14,26 +27,95 @@ const shoeData = [
     stripe_price_id: 'price_1KaTXwDT393wRvxWAah2oY1Z',
   },
   {
-    name: 'Adidas Yeezy Boost 350 V2 Beluga',
+    name: 'Adidas Yeezy Boost 350 V2 Beluga 2.0',
     price: 575,
-    description: `The adidas Yeezy 350 Boost V2 takes the silhouette that 
-        Kanye West made famous for adidas Originals and gives it new life.
-        The low top Yeezy Boost 350 features a full length Boost cushioning system and a high end feel 
-        that has made it the go-to sneaker for everyone from celebrities like Kim Kardashian-West, 
-        Jay-Z, 2 Chainz and Future, as well as for athletes such as Nick "Swaggy P" Young, Andrew Wiggins, 
-        Lewis Hamilton and anyone else who can get their feet in a pair.`,
+    description: `The Yeezy Beluga 2.0 takes Kanye West's famous adidas sneakers full circle by returning to the 
+    colors of the first adidas Yeezy Boost 350 V2. While the original colorway featured a bright orange 
+    (officially Solar Red) stripe across the side, the 2017 release of the Yeezy Beluga colorway is much more subtle. 
+    This colorway utilizes the same Zebra-like stripes as the original but the SPLY-350 branding is the only part of the 
+    upper with a contrasted color-this time called Bold Orange.`,
     brand: 'Adidas',
-    year: 2016,
+    year: 2017,
     filename: 'Beluga2.jpg',
     stripe_prod_id: 'prod_LH1bvRSEvkLJNm',
     stripe_price_id: 'price_1KaTYKDT393wRvxWX8bF31Hm',
   },
   {
+    name: 'New Balance 550 Shadow',
+    price: 115,
+    description: `The New Balance 550 Shadow features a black and grey leather upper with mesh paneling around the collars. 
+    A puffed New Balance logo and "550" branding on the quarter panels nods to the origins of New Balance's basketball line.`,
+    year: '2021',
+    filename: 'Black550.jpg',
+    stripe_prod_id: '',
+    stripe_price_id: '',
+  },
+  {
+    name: 'Air Jordan 6 Retro Carmine',
+    price: 270,
+    description: `The Air Jordan 6 Carmine returned in its original form in 2021. The nearly 20-year-old colorway 
+    first made its appearance in 1991 during Michael Jordan’s ’91-’92 season and brought in a new form of color 
+    blocking to the signature silhouette. The Air Jordan 6 Carmine combines red, white, and black colors to 
+    represent the Chicago Bulls’ signature colors. The upper features bold red and white colors that help the 
+    model pop while on foot. From there, the upper is met with a white and black midsole, with hits of an icy blue 
+    outsole appear as well. The final touch to this OG’s return is the signature “Nike Air” on the heel, replacing 
+    2008 and 2012 retros’ “Jumpman” logo.`,
+    year: '2021',
+    filename: 'Carmines.jpg',
+    stripe_prod_id: '',
+    stripe_price_id: '',
+  },
+  {
+    name: 'Air Jordan 13 Retro Flint',
+    price: 300,
+    description: `With previous retros in 2005 and 2010, this is the third time that the Jordan 13 Flint has been 
+    reintroduced. This Jordan 13 Retro consists of a navy mesh and white leather upper with flint grey suede underlays. 
+    A Jumpman holograph on the ankle, 3M detailing throughout the upper, and Carolina blue branding on the tongue completes 
+    the design.`,
+    year: '2020',
+    filename: 'Flint.jpg',
+    stripe_prod_id: '',
+    stripe_price_id: '',
+  },
+  {
+    name: 'Air Jordan 1 Retro High OG Light Fusion Red',
+    price: 160,
+    description: `The Air Jordan 1 Light Fusion Red features a white leather upper with Light Fusion Red overlays and 
+    Laser Orange on the Swoosh and ankle wrap. A woven Nike label stitched atop the tongue completes the design.`,
+    year: '2021',
+    filename: 'Fusion.jpg',
+    stripe_prod_id: '',
+    stripe_price_id: '',
+  },
+  {
+    name: 'Air Jordan 6 Retro Gold Hoops',
+    price: 200,
+    description: `The women's Air Jordan 6 Gold Hoops (W) draws inspiration from gold hoop earrings and their allure. 
+    Its design is composed of a white tumbled leather upper with hints of pale pink on the tongue. From there, interconnected 
+    gold hoops and Jumpman emblems are attached to the shoe’s signature lace lock`,
+    year: '2021',
+    filename: 'Hoops.jpg',
+    stripe_prod_id: '',
+    stripe_price_id: '',
+  },
+  {
+    name: 'New Balance 990v3 x JJJJound',
+    price: 400,
+    description: `JJJJound and New Balance teamed up again for their second 990v3 design with the New Balance 990v3 JJJJound Olive.
+    Arriving with olive mesh and premium suede uppers, the New Balance 990v3 JJJJound Olive uses a minimalist approach to create 
+    a shoe geared for everyday wear. Black New Balance logos and overlays bring sharp contrast to the tonal green background. A silver 
+    reflective heel wrap with JJJJound branding adds a custom touch. From there, a cream ENCAP sole at the base completes the design.`,
+    year: '2022',
+    filename: 'JJJJound.jpg',
+    stripe_prod_id: '',
+    stripe_price_id: '',
+  },
+  {
     name: 'Air Jordan 1 x SB LA to Chicago',
     price: 590,
     description: `This AJ 1 SB comes with a white upper plus blue and yellow accents, 
-        yellow Nike "Swoosh", white midsole, and a black sole. Underneath the purple and yellow coloring
-        you can find some other colors, but only when the paint is irritated. GET TO SKATING!!!`,
+    yellow Nike "Swoosh", white midsole, and a black sole. Underneath the purple and yellow coloring
+    you can find some other colors, but only when the paint is irritated. GET TO SKATING!!!`,
     brand: 'Jordan',
     year: 2019,
     filename: 'LaToChi.jpg',
@@ -81,12 +163,24 @@ const shoeData = [
     stripe_price_id: 'price_1KaWN8DT393wRvxWyj3DCHEI',
   },
   {
+    name: 'Air Jordan 1 Retro High Royal Toe',
+    price: 300,
+    description: `The Royal Toe takes inspiration from the original Jordan 1 Royal colorway 
+    (which released in 1985) and adds design elements of the Black Toe 1 to create a familiar, 
+    but new dialogue. This AJ 1 features a white and royal leather upper with black leather overlays 
+    and detailing. A white midsole, royal outsole, and branded leather tongue tag completes this design.`,
+    year: '2020',
+    filename: 'RoyalToe.jpg',
+    stripe_prod_id: '',
+    stripe_price_id: '',
+  },
+  {
     name: 'Air Jordan 1 Shattered Backboard',
     price: 1600,
     description: `The name “Shattered Backboard” comes from a moment after Michael Jordans 
-        rookie season, in the summer of 1985, when he played in an exhibition game in Italy. 
-        Jordan, wearing the orange, black, and white of Stefanel Trieste, hit a dunk so hard 
-        he shattered the glass backboard.`,
+    rookie season, in the summer of 1985, when he played in an exhibition game in Italy. 
+    Jordan, wearing the orange, black, and white of Stefanel Trieste, hit a dunk so hard 
+    he shattered the glass backboard.`,
     brand: 'Jordan',
     year: 2015,
     filename: 'SBB.jpg',
@@ -94,13 +188,24 @@ const shoeData = [
     stripe_price_id: 'price_1KaWNVDT393wRvxWLW6ULrgi',
   },
   {
+    name: 'Air Jordan 1 Retro High OG Seafoam',
+    price: 270,
+    description: `The women's Air Jordan 1 High Seafoam (W) arrives in a smooth white leather 
+    construction with Seafoam Durabuck overlays and Swooshes. On the laces, crisp bronze trimming 
+    stands out against the design's subtle coloration.`,
+    year: '2021',
+    filename: 'Seafoam.jpg',
+    stripe_prod_id: '',
+    stripe_price_id: '',
+  },
+  {
     name: 'Nike x Stussy Air Zoom Spiridon Fossil',
     price: 565,
     description: `Stussy teams up with Nike once again to deliver the Nike Air Zoom Spiridon 
-        Cage 2 Stussy Fossil. After originally debuting in 2003, this design received a refresh 
-        with the help of lifestyle label Stussy. This Spridion Cage 2 consists of a mesh upper with 
-        woven textile overlays and a black Swoosh. A caged Zoom Air unit, Stussy branding on the heel 
-        and toe, and Fossil sole completes the design.`,
+    Cage 2 Stussy Fossil. After originally debuting in 2003, this design received a refresh 
+    with the help of lifestyle label Stussy. This Spridion Cage 2 consists of a mesh upper with 
+    woven textile overlays and a black Swoosh. A caged Zoom Air unit, Stussy branding on the heel 
+    and toe, and Fossil sole completes the design.`,
     brand: 'Nike',
     year: 2020,
     filename: 'Spiridon.jpg',
@@ -131,6 +236,30 @@ const shoeData = [
     filename: 'SupAF1.jpg',
     stripe_prod_id: 'prod_LH4YJxZbpzzfsC',
     stripe_price_id: 'price_1KaWP8DT393wRvxWYgEoTnOv',
+  },
+  {
+    name: 'Nike Air Max 98 x Supreme Snakeskin',
+    price: 450,
+    description: `The Supreme x Air Max 98 'Snakeskin' puts snakeskin detailing on its overlays. 
+    The rest of the shoe incorporates a metallic silver mesh with reflective piping, while the 
+    heel tab and toe sport Supreme branding, with 'World Famous' across the heels.`,
+    year: '2016',
+    filename: 'SupAM98.jpg',
+    stripe_prod_id: '',
+    stripe_price_id: '',
+  },
+  {
+    name: 'New Balance 550 UNC White University Blue',
+    price: 175,
+    description: `New Balance has a handful of firm favorites among the sneakerhead community, 
+    but none have taken off quite like its 550 silhouette. As expected, the retro Basketball 
+    Oxford shape is presented in white to show off its OG shapes and style, complete with a 
+    padded “N” emblem on the mid-panel alongside old-school branding on the tongue, “NB” emblems 
+    on the rear, and “550” gracing the vamp.`,
+    year: '2022',
+    filename: 'UNC550.jpg',
+    stripe_prod_id: '',
+    stripe_price_id: '',  
   },
   {
     name: 'Adidas Yeezy Boost 700 Wave Runner',
